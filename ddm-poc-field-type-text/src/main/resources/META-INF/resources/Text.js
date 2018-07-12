9e5644dd6ce88d9ddf949afbe071209025100df2
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-07-12 11:38:30 -0300

    Declare the STATE of the Text field

--- ddm-poc-field-type-text/src/main/resources/META-INF/resources/Text.js
+++ ddm-poc-field-type-text/src/main/resources/META-INF/resources/Text.js
@@ -7,11 +7,63 @@ import templates from './Text.soy.js';
 import TextRegister from './TextRegister.soy.js';
 
 class Text extends Component {
-    created() {
-        console.log('FieldType: Text')
-    }
+    static STATE = {
+        /**
+         * @default false
+         * @instance
+         * @memberof Text
+         * @type {?bool}
+         */
+        editable: Config.bool().value(false),
 
-    static STATE = {}
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Text
+         * @type {?(string|undefined)}
+         */
+        id: Config.string(),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Text
+         * @type {?(string|undefined)}
+         */
+        label: Config.string(),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Text
+         * @type {?(string|undefined)}
+         */
+        placeholder: Config.string(),
+        
+        /**
+         * @default false
+         * @instance
+         * @memberof Text
+         * @type {?(bool|undefined)}
+         */
+        required: Config.bool().value(false),
+
+        /**
+         * @default true
+         * @instance
+         * @memberof Text
+         * @type {?(bool|undefined)}
+         */
+        showLabel: Config.bool().value(true),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Text
+         * @type {?(string|undefined)}
+         */
+        spritemap: Config.string(),
+    }
 }
 
 Soy.register(Text, templates);
