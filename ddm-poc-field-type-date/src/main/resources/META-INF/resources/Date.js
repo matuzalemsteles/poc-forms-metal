3b95806db169c00c7c69289702e0fb7e2e586604
Matuzalém S. Teles <matuzalemteles@gmail.com>
2018-07-12 11:57:25 -0300

    Declare the STATE of the Date field

--- ddm-poc-field-type-date/src/main/resources/META-INF/resources/Date.js
+++ ddm-poc-field-type-date/src/main/resources/META-INF/resources/Date.js
@@ -7,8 +7,36 @@ import Soy from 'metal-soy';
 import templates from './Date.soy.js';
 import DateRegister from './DateRegister.soy.js';
 
+/**
+ * Date.
+ * @extends Component
+ */
 class Date extends Component {
     static STATE = {
+        /**
+         * @default false
+         * @instance
+         * @memberof Date
+         * @type {?bool}
+         */
+        editable: Config.bool().value(false),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Date
+         * @type {?(string|undefined)}
+         */
+        id: Config.string(),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Date
+         * @type {?(string|undefined)}
+         */
+        label: Config.string(),
+
         /**
          * @default __/__/____
          * @instance
@@ -18,16 +46,36 @@ class Date extends Component {
         placeholder: Config.string().value('__/__/____'),
 
         /**
+         * @default false
+         * @instance
+         * @memberof Date
+         * @type {?bool}
+         */
+        required: Config.bool().value(false),
+
+        /**
+         * @default true
+         * @instance
+         * @memberof Date
+         * @type {?bool}
+         */
+        showLabel: Config.bool().value(true),
+
+        /**
+         * @default undefined
+         * @instance
+         * @memberof Date
+         * @type {?(string|undefined)}
+         */
+        spritemap: Config.string(),
+
+        /**
          * @default undefined
          * @instance
          * @memberof Date
          * @type {?(string|undefined)}
          */
         value: Config.string(),
-    }
-
-    created() {
-        console.log('FieldType: Date')
     }
 }
 
