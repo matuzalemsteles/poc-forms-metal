f2e2884212ac736eb503519aa026dacf80e37471
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-07-30 17:18:32 -0300

    Add Grid into ddm-poc-form-field-type

---
+++ ddm-poc-form-field-type/src/main/resources/META-INF/resources/Grid/__tests__/Grid.js
@@ -0,0 +1,94 @@
+import Grid from '../Grid';
+
+
+let component;
+let spritemap = 'icons.svg';
+
+describe('Grid', () => {
+    afterEach(() => {
+        if (component) {
+			component.dispose();
+        }
+    });
+
+    it('should render columns', () => {
+        component = new Grid({
+            columns: [{label: 'col1', value:'fieldId'}, {label: 'col2', value:'fieldId'}],
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should be not edidable', () => {
+        component = new Grid({
+            editable: false,
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a helpText', () => {
+        component = new Grid({
+            helpText: 'Type something',
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have an id', () => {
+        component = new Grid({
+            id: 'ID',
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a label', () => {
+        component = new Grid({
+            label: 'label',
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should not be required', () => {
+        component = new Grid({
+            required: false,
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should render rows', () => {
+        component = new Grid({
+            rows: [{label: 'row1', value:'fieldId'}, {label: 'row2', value:'fieldId'}],
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should render Label if showLabel is true', () => {
+        component = new Grid({
+            label: 'text',
+            showLabel: true,
+            spritemap: spritemap,
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a spritemap', () => {
+        component = new Grid({
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+});
