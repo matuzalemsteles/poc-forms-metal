ba6c30ddc436fce26af7015975264744709880b9
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-07-30 17:16:14 -0300

    Add Checkbox into ddm-poc-form-field-type

---
+++ ddm-poc-form-field-type/src/main/resources/META-INF/resources/Checkbox/__tests__/Checkbox.js
@@ -0,0 +1,135 @@
+import Checkbox from '../Checkbox';
+import {dom as MetalTestUtil} from 'metal-dom';
+
+let component;
+let spritemap = 'icons.svg';
+
+describe('Field Checkbox', () => {
+    afterEach(() => {
+        if (component) {
+			component.dispose();
+        }
+    });
+
+    it('should be not edidable', () => {
+        component = new Checkbox({
+            editable: false,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a helptext', () => {
+        component = new Checkbox({
+            helpText: 'Type something',
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have an id', () => {
+        component = new Checkbox({
+            id: 'ID',
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a label', () => {
+        component = new Checkbox({
+            label: 'label',
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a predefined Value', () => {
+        component = new Checkbox({
+            placeholder: 'Option 1',
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should not be required', () => {
+        component = new Checkbox({
+            required: false,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should be shown as a switcher', () => {
+        component = new Checkbox({
+            showAsSwitcher: true,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should be shown as checkbox', () => {
+        component = new Checkbox({
+            showAsSwitcher: false,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should render Label if showLabel is true', () => {
+        component = new Checkbox({
+            label: 'text',
+            showLabel: true,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a spritemap', () => {
+        component = new Checkbox({
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a value', () => {
+        component = new Checkbox({
+            value: true,
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should have a key', () => {
+        component = new Checkbox({
+            key: 'key',
+            spritemap: spritemap
+        });
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should emit field edit event on field change', () => {
+        const handleFieldChange = jest.fn();
+        const events = {fieldEdit: handleFieldChange};
+        component = new Checkbox({
+            spritemap: spritemap,
+            events
+        });
+
+        MetalTestUtil.triggerEvent(component.element.querySelector('input'), 'change', {});
+
+        expect(handleFieldChange).toHaveBeenCalled();
+    });
+
+});
